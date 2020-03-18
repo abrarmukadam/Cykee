f4713e672575c2e219d8547207c52a1e71fa70b0
Abrar Mukadam <abrar@Abrars-MacBook-Air.local>
2020-03-18 22:27:19 +0400

    RN Camera added to the CameraScreen

--- components/screens/CameraScreen/CameraScreen.js
+++ components/screens/CameraScreen/CameraScreen.js
@@ -1,13 +1,51 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, TouchableOpacity} from 'react-native';
 import styles from './styles';
+
+import {RNCamera} from 'react-native-camera';
 
 class CameraScreen extends Component {
   state = {};
+  takePicture = async () => {
+    if (this.camera) {
+      const options = {quality: 0.5, base64: true};
+      const data = await this.camera.takePictureAsync(options);
+      console.log(data.uri);
+    }
+  };
   render() {
     return (
       <View style={styles.container}>
-        <Text> THIS IS CAMERA LAUNCH SCREEN</Text>
+        <RNCamera
+          ref={ref => {
+            this.camera = ref;
+          }}
+          style={styles.preview}
+          type={RNCamera.Constants.Type.back}
+          flashMode={RNCamera.Constants.FlashMode.on}
+          androidCameraPermissionOptions={{
+            title: 'Permission to use camera',
+            message: 'We need your permission to use your camera',
+            buttonPositive: 'Ok',
+            buttonNegative: 'Cancel',
+          }}
+          androidRecordAudioPermissionOptions={{
+            title: 'Permission to use audio recording',
+            message: 'We need your permission to use your audio',
+            buttonPositive: 'Ok',
+            buttonNegative: 'Cancel',
+          }}
+          onGoogleVisionBarcodesDetected={({barcodes}) => {
+            console.log(barcodes);
+          }}
+        />
+        <View style={{flex: 0, flexDirection: 'row', justifyContent: 'center'}}>
+          <TouchableOpacity
+            onPress={this.takePicture.bind(this)}
+            style={styles.capture}>
+            <Text style={{fontSize: 14}}> SNAP </Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
--- components/screens/CameraScreen/styles.js
+++ components/screens/CameraScreen/styles.js
@@ -4,9 +4,22 @@ const {width: WIDTH} = Dimensions.get('window');
 
 export default StyleSheet.create({
   container: {
-    alignItems: 'center',
     flex: 1,
-    backgroundColor: 'grey',
-    justifyContent: 'center',
+    flexDirection: 'column',
+    backgroundColor: 'black',
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: 'center',
+    margin: 20,
   },
 });
